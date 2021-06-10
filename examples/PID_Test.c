57464dbe7dce3db7ac85dcf06e815b0ad2dfc47f
Talha Sarı <talhasari4343@outlook.com.tr>
2021-06-10 11:26:17 +0300

    fixed sim params

--- examples/PID_Test.c
+++ examples/PID_Test.c
@@ -7,26 +7,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <PID.h>
+#include "../src/PID.h"
 
-#define PID_KP 2.0f
-#define PID_KD 0.5f
-#define PID_KI 0.25f
+#define PID_KP 2.0F
+#define PID_KD 0.5F
+#define PID_KI 0.25F
+#define SIMULATION_TIME_MAX 10.0F
 
 float TestSim_Update(float inp);
 
 int main()
 {
+    PID_t pid;
 
-    PIDController_Init(&pid, PID_KP, PID_KI, PID_KD);
+    PID_Init(&pid, PID_KP, PID_KI, PID_KD, 0.01f);
+    PID_SetLimits(&pid, 0.0f, 100.0f, -100.0f, 100.0f);
+    PID_SetDerivativeFilter(&pid, 0.02f);
 
-    uint16_t setpoint = 21;
+    float setpoint = 21.0f;
 
     printf("T (s)\tSystem Out\tPID Out\r\n");
     for (float t = 0.0f; t <= SIMULATION_TIME_MAX; t += SAMPLE_TIME_S)
     {
         float meas = TestSim_Update(pid.out);
-        PIDController_Update(&pid, setpoint, meas);
+        PID_Update(&pid, setpoint, meas);
         printf("%f\t%f\t%f\r\n", t, meas, pid.out);
     }
     
