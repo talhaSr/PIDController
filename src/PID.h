bc29dce729069ebe315aa0b10dedd89efdb6ff82
Talha Sarı <talhasari4343@outlook.com.tr>
2021-06-10 11:25:41 +0300

    major fix
    added two more functions and re-organized, document info added

--- src/PID.h
+++ src/PID.h
@@ -1,12 +1,18 @@
-/*
- * PID.h
- *
- *		Revision Date: 13/01/2021
- *		Author: Talha
+/**
+ * @file PID.h
+ * @author Talha Sarı (talha.sari@outlook.com.tr)
+ * @brief PID Controller for Discrete Time Systems
+ * @version v1.1
+ * @date 2021-06-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
  */
 
 #ifndef INC_PID_H_
 #define INC_PID_H_
+
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,7 +24,6 @@ extern "C" {
 #define PID_LIM_MIN_INT -100.0f
 #define PID_LIM_MAX_INT 100.0f
 #define SAMPLE_TIME_S 0.01f
-
 
 typedef struct {
 
@@ -50,11 +55,46 @@ typedef struct {
 	//Kontrolcü Çıkışı
 	float out;
 
-} PIDKontrolcu_t;
+} PID_t;
 
-void PIDKontrolcu_Init(PIDKontrolcu_t *pid);
-int16_t PIDKontrolcu_Update(PIDKontrolcu_t *pid, uint16_t setPoint, uint16_t measurement);
+/**
+ * @brief PID Controller Initializer
+ * 
+ * @param pid PID Handler TypeDef
+ * @param kp  Proportional
+ * @param ki  Integral
+ * @param kd  Derivative
+ */
+void PID_Init(PID_t *pid, float kp, float ki, float kd, float Ts);
 
+/**
+ * @brief PID Controller Updater
+ * 
+ * @param pid		  PID Handler TypeDef
+ * @param setPoint    Set (Reference) Point
+ * @param measurement Measurement Value - x[n]
+ * @return float 
+ */
+float PID_Update(PID_t *pid, float setPoint, float measurement);
+
+/**
+ * @brief PID Output and Integral Anti-windup Limiting Function
+ * 
+ * @param pid 		PID Handler TypeDef
+ * @param outLimMin Minimum Signal Output
+ * @param outLimMax Maximum Signal Output
+ * @param intLimMin Minimum Integral Anti-windup Value
+ * @param intLimMax Maximum Integral Anti-windup Value
+ */
+void PID_SetLimits(PID_t *pid, float outLimMin, float outLimMax, float intLimMin, float intLimMax);
+
+/**
+ * @brief PID Derivative's Filter Set for Noise
+ * 
+ * @param pid PID Handler TypeDef
+ * @param tau Filter Coefficient
+ */
+void PID_SetDerivativeFilter(PID_t *pid, float tau);
 
 #ifdef __cplusplus
 }
